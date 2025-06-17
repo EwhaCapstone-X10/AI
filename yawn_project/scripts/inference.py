8d40dcb9b82dfbcf496115db633e2564c9d3e56d
minjeon99 <144228580+minjeon99@users.noreply.github.com>
2025-06-17 15:29:02 +0900

    model load: huggingface

--- yawn_project/scripts/inference.py
+++ yawn_project/scripts/inference.py
@@ -4,10 +4,8 @@ import librosa
 from transformers import ASTFeatureExtractor, ASTForAudioClassification
 
 # (global) model loading
-MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "model", "yawn_AST_bin"))
-
 print("📦 모델 로드 중")
-yawn_model = ASTForAudioClassification.from_pretrained(MODEL_PATH, use_safetensors=False)
+yawn_model = ASTForAudioClassification.from_pretrained("minjeon99/yawn-ast-bin")
 print("✅ 모델 로딩 완료")
 yawn_fe = ASTFeatureExtractor.from_pretrained(MODEL_PATH)
 yawn_model.eval()
